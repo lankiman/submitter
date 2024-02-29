8e49b18ac246181cb3a615514e284cb2ac052a02
root <marvellousgboun@gmail.com>
2024-02-29 13:49:44 +0100

    still making changes

--- frontend/index.js
+++ frontend/index.js
@@ -68,17 +68,76 @@ dragDrop.addEventListener("drop", (e) => {
   fileInput.dispatchEvent(new Event("change"));
 });
 
+//custom input functionality
+chooseFileButton.addEventListener("click", () => {
+  fileInput.click();
+});
+chooseFileIcon.addEventListener("click", () => {
+  fileInput.click();
+});
+
 fileInput.addEventListener("change", () => {
   const files = fileInput.files;
 
   emptyError.style.display = "none";
   namingError.style.display = "none";
 
-  for (let inputFile of files) {
-    // Add the file to the selectedFiles array
+  // for (let inputFile of files) {
+  //   // Add the file to the selectedFiles array
 
-    const li = document.createElement("li");
-    li.innerHTML = `
+  //   const li = document.createElement("li");
+  //   li.innerHTML = `
+  //   <p class="file--name">${inputFile.name}</p>
+  //   <button class="del--button" type="button">
+  //     <svg
+  //       data-slot="icon"
+  //       fill="none"
+  //       stroke-width="1.5"
+  //       stroke="currentColor"
+  //       viewBox="0 0 24 24"
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       aria-hidden="true"
+  //     >
+  //       <path
+  //         stroke-linecap="round"
+  //         stroke-linejoin="round"
+  //         d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+  //       ></path>
+  //     </svg></button
+  //   >`;
+
+  //   switch (selectedFiles.length > 0) {
+  //     case true:
+  //       let fileExists = false;
+  //       selectedFiles.forEach((file) => {
+  //         if (file.name === inputFile.name) {
+  //           fileExists = true;
+  //         }
+  //       });
+  //       if (!fileExists) {
+  //         selectedFiles.push(inputFile);
+  //         filesList.appendChild(li);
+  //       }
+  //       break;
+  //     case false:
+  //       selectedFiles.push(inputFile);
+  //       filesList.appendChild(li);
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  for (let inputFile of files) {
+    const fileIndex = selectedFiles.findIndex(
+      (file) => file.name === inputFile.name
+    );
+
+    if (fileIndex !== -1) {
+      selectedFiles.splice(fileIndex, 1, inputFile);
+    } else {
+      selectedFiles.push(inputFile);
+
+      const li = document.createElement("li");
+      li.innerHTML = `
     <p class="file--name">${inputFile.name}</p>
     <button class="del--button" type="button">
       <svg
@@ -97,11 +156,10 @@ fileInput.addEventListener("change", () => {
         ></path>
       </svg></button
     >`;
-
-    selectedFiles.push(inputFile);
-    filesList.appendChild(li);
-    fileInput.value = "";
+      filesList.appendChild(li);
+    }
   }
+  console.log(selectedFiles);
   let delButton = document.querySelectorAll(".del--button");
   delButton.forEach((button) => {
     button.addEventListener("click", () => {
@@ -123,14 +181,6 @@ selectInput.addEventListener("change", () => {
     selectError.style.display = "none";
     selectInput.style.borderColor = "initial";
   }
-});
-
-//custom input functionality
-chooseFileButton.addEventListener("click", () => {
-  fileInput.click();
-});
-chooseFileIcon.addEventListener("click", () => {
-  fileInput.click();
 });
 
 //form submision and validation
